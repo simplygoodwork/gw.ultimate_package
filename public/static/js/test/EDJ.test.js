cf12804c77cb86ea36a1fd2e255cb9e053100929
Wil Linssen <wil@linssen.me>
2013-02-06 21:38:07 +0000

    tests for some more EDJ methods

--- public/static/js/test/EDJ.test.js
+++ public/static/js/test/EDJ.test.js
@@ -1,3 +1,33 @@
 test( "Set up test", function() {
-    equal(typeof(EDJ.settings), "object", "EDJ has not been instantiated");
+    equal(typeof(EDJ), "function", "EDJ has not been instantiated");
+    equal(typeof(EDJ.settings), "object", "EDJ has no settings");
+});
+
+test("Logging", function() {
+
+    equal(typeof(EDJ.log), "function", "Can't find the log method");
+
+    EDJ.settings.debug = true;
+    equal(EDJ.log("testing EDJ.log"), undefined);
+
+    EDJ.settings.debug = false;
+    equal(EDJ.log("testing EDJ.log"), false)
+});
+
+test("Navigation", function() {
+    var $accessLinks = $("<ul id='nav_access'><li><a href='#qunit'>qunit</a></li></ul>");
+
+    equal(EDJ.navigation.run(), false); 
+
+    $("body").prepend($accessLinks);
+    equal(EDJ.navigation.run(), true, "Navigation didn't run");
+    EDJ.navigation.init();
+
+    $accessLinks.find("a").trigger("focus");
+    ok($accessLinks.find("a").hasClass("focus"), "Link never got the focus class");
+
+    $accessLinks.find("a").trigger("click");
+    ok($("#qunit:focus").length > 0, "Link's target didn't get focus after click");
+
+    $accessLinks.remove();
 });
